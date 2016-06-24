aedd6a0e502a07257a95be4abf7e3d2fa04134f9
David Silva <davidsilva3290@gmail.com>
2016-06-24 00:12:14 -0500

    oli

---
+++ Proyecto/UPCNETSystemCliente/UPCNETSystemCliente/Controllers/ProfesorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UPCNETSystemCliente.ViewModel.Profesor;
+
+namespace UPCNETSystemCliente.Controllers
+{
+    public class ProfesorController : Controller
+    {
+        public ActionResult Profesor()
+        {
+            return RedirectToAction("Horario");
+        }
+
+        public ActionResult Horario() 
+        {
+            HorarioViewModel objViewModel = new HorarioViewModel();
+            string id = Session["IDProfesor"].ToString();
+            objViewModel.Fill(id);
+            return View(objViewModel);
+        }
+
+        public ActionResult LstCurso()
+        {
+            LstCursoViewModel objViewModel = new LstCursoViewModel();
+            objViewModel.Fill(Session["IDProfesor"].ToString());
+            return View(objViewModel);
+        }
+
+        public ActionResult LstAlumno(string idCurso)
+        {
+            LstAlumnoViewModel objViewModel = new LstAlumnoViewModel();
+            objViewModel.Fill(idCurso);
+            return View(objViewModel);
+        }
+    }
+}
